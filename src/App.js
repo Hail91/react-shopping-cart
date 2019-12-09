b919155627f22feb13d41ce9abefea53a73c0e70
Aaron <aarongillies88@gmail.com>
2019-12-09 17:47:38 -0500

    Quick attempt at stretch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-import Product from './components/Product';
 
 function App() {
 	const [products] = useState(data);
